9525af6427b7c2508edcb8eb8b25673ef185224c
subramaniashiva <vengaishiva@gmail.com>
2015-08-14 08:02:39 +0530

    Added event listeners

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,12 @@
 // jshint devel:true
 (function(){
   console.log('initiated');
+  Date.prototype.addDays = function (n) {
+    var time = this.getTime();
+    var changedDate = new Date(time + (n * 24 * 60 * 60 * 1000));
+    this.setTime(changedDate.getTime());
+    return this;
+  };
   // AJAX crawling
   function callAjax(url, callback, params) {
       var xmlhttp;
@@ -23,5 +29,25 @@
     var currDate = new Date(date.startTime);
     console.log("current date is ", currDate);
   }
+  function showMeetingsForDate(dateString) {
+    console.log("The date passed is ", dateString);
+  }
+  var prev = document.getElementById("prev"),
+      next = document.getElementById("next"),
+      today = document.getElementById("today");
+  prev.addEventListener("click", function() {
+    var date = new Date();
+    date = date.addDays(-1);
+    showMeetingsForDate(date.toString());
+  });
+  next.addEventListener("click", function() {
+    var date = new Date();
+    date = date.addDays(1);
+    showMeetingsForDate(date.toString());
+  });
+  today.addEventListener("click", function() {
+    var date = new Date();
+    showMeetingsForDate(date.toString());
+  });
   callAjax("data/sample-data.json", processMeetingData);
 })();
